cbdc1a8af2db3de0a001e9d38644897785b5612f
omo5236@g.rit.edu <omo5236@g.rit.edu@cda44fc4-ffb3-9101-b382-7fd7d470b8d5>
2012-11-10 01:30:14 +0000

    Cleanup on service

--- Test/src/DataLayer/Service.java
+++ Test/src/DataLayer/Service.java
@@ -2,38 +2,103 @@ package DataLayer;
 
 import java.util.*;
 
-public class Service
-{
-	// Provide	attributes	that	mirror	the	Services	table.
-	private String userId;
+public class Service {
+	
+        private MySQLDatabase myDB = new MySQLDatabase();
+	
+    
+        private String userId;
 	private String serviceId;
 	private String year;
 	private String description;
 	private String role;
-        private MySQLDatabase myDB = new MySQLDatabase();
-	
-	// Provide	a	default	constructor.
+        
 	public Service()
 	{
 	}
 	
 	// Provide	a	constructor	that	accepts	and	sets	the	serviceId.
-	public Service(String serviceId)
-	{
+	public Service(String serviceId){
 		this.serviceId = serviceId;
 	}
 	
 	// Provide	a	constructor	that	accepts	and	sets	all	attributes.
-	public Service(String userId, String serviceId, String year, String description, String role)
-	{
+	public Service(String userId, String serviceId, String year, String description, String role){
 		this.userId = userId;
 		this.serviceId = serviceId;
 		this.year = year;
 		this.description = description;
 		this.role = role;
 	}
+
+        // fetch	uses	the	object�s	serviceId	attribute	and	the	Database	class
+	//getData	method	to	retrieve	the	database	values	for	that	particular	serviceId	
+	//and	updates	the	object�s	attributes.
+	public boolean fetch() throws DLException {
+            try{                
+		ArrayList<String> values = new ArrayList<String>(0);
+		values.add(serviceId);
+		ArrayList<ArrayList<String>> dataList = myDB.getData("SELECT * FROM services WHERE ServiceId = ?", values);
+		if(dataList != null){
+                    userId = dataList.get(1).get(1).toString();
+                    serviceId = dataList.get(1).get(2).toString();
+                    year = dataList.get(1).get(3).toString();
+                    description = dataList.get(1).get(4).toString();
+                    role = dataList.get(1).get(5).toString();
+                    return true;
+		} else {
+			return false;
+		}
+            } catch (Exception e){
+                throw new DLException(e);
+            }
+	}
 	
-	// Provide	accessors	and	mutators	for	all	attributes.
+	// post updates the database values, for that object�s serviceId, using the	
+	//object�s attribute values.
+	public boolean post() throws DLException {
+            try {
+		ArrayList<String> values = new ArrayList<String>(0);
+		values.add(userId); 
+		values.add(year);
+		values.add(description);
+		values.add(role);
+		values.add(serviceId);
+		return myDB.setData("UPDATE service UserId = ?, Year = ?, Description = ?, Role = ? WHERE ServiceId = ?", values);
+            } catch(Exception e) {
+                throw new DLException(e);
+            }
+        }
+	
+	// put	inserts	the	object�s	attribute	values	into	the	database	as	a	new	record.
+	public boolean put() throws DLException {
+            try {
+		ArrayList<String> values = new ArrayList<>(0);
+		values.add(userId);
+		values.add(serviceId);
+		values.add(year);
+		values.add(description);
+		values.add(role);
+        	myDB.setData("INSERT INTO service (UserId,ServiceId,Year,Description,Role) VALUES(?,?,?,?,?)", values);
+                return true;
+            } catch(Exception e) {
+                throw new DLException(e);
+            }
+        }
+	
+    // delete removes	from	the	database	any	data	corresponding	to	the	object�s serviceId.
+    public boolean delete() throws DLException {
+        try {
+            ArrayList<String> values = new ArrayList<String>(0);
+            values.add(serviceId);
+            myDB.setData("DELETE FROM service WHERE serviceID = ?", values);
+            return true;
+        } catch(Exception e) {
+            throw new DLException(e);
+        }
+    }
+    
+        // Provide	accessors	and	mutators	for	all	attributes.
 	public void setServiceId(String serviceId){this.serviceId = serviceId;}
 	public String getServiceId(){return serviceId;}
 	
@@ -49,93 +114,5 @@ public class Service
 	public void setRole(String role){this.role = role;}
 	public String getRole(){return role;}
 	
-	// fetch	uses	the	object�s	serviceId	attribute	and	the	Database	class
-	//getData	method	to	retrieve	the	database	values	for	that	particular	serviceId	
-	//and	updates	the	object�s	attributes.
-	public boolean fetch() throws DLException
-	{
-            try{    
-            myDB = new MySQLDatabase();
-            
-		ArrayList<String> values = new ArrayList<String>(0);
-		values.add(serviceId);
-		ArrayList<ArrayList<String>> dataList = myDB.getData("SELECT * FROM services WHERE ServiceId = ?", values);
-		if(dataList != null)
-		{
-			userId = dataList.get(1).get(1).toString();
-			serviceId = dataList.get(1).get(2).toString();
-			year = dataList.get(1).get(3).toString();
-			description = dataList.get(1).get(4).toString();
-			role = dataList.get(1).get(5).toString();
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-            } catch (Exception e){
-                throw new DLException(e);
-            }
-	}
-	
-	// post updates the database values, for that object�s serviceId, using the	
-	//object�s attribute values.
-	public boolean post() throws DLException
-	{
-            myDB = new MySQLDatabase();
 
-            try
-            {
-		ArrayList<String> values = new ArrayList<String>(0);
-		values.add(userId); 
-		values.add(year);
-		values.add(description);
-		values.add(role);
-		values.add(serviceId);
-		return myDB.setData("UPDATE service UserId = ?, Year = ?, Description = ?, Role = ? WHERE ServiceId = ?", values);
-            }
-            catch(DLException e)
-            {
-                return false;
-            }
-        }
-	
-	// put	inserts	the	object�s	attribute	values	into	the	database	as	a	new	record.
-	public boolean put() throws DLException
-	{
-            myDB = new MySQLDatabase();
-
-            
-            try
-            {
-		ArrayList<String> values = new ArrayList<String>(0);
-		values.add(userId);
-		values.add(serviceId);
-		values.add(year);
-		values.add(description);
-		values.add(role);
-                myDB.connect();
-		myDB.setData("INSERT INTO service (UserId,ServiceId,Year,Description,Role) VALUES(?,?,?,?,?)", values);
-                myDB.close();
-                return true;
-            }
-            catch(DLException e)
-            {
-                return false;
-            }
-        }
-	
-    // delete removes	from	the	database	any	data	corresponding	to	the	object�s serviceId.
-    public boolean delete() throws DLException
-    {
-        myDB = new MySQLDatabase();
-        try {
-            ArrayList<String> values = new ArrayList<String>(0);
-            values.add(serviceId);
-            myDB.setData("DELETE FROM service WHERE serviceID = ?", values);
-            return true;
-        } catch(DLException e) {
-            return false;
-        }
-    }
 }
