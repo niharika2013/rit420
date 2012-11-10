e139eae125c38341d754b150b2fb60057b332c1c
acm8017@g.rit.edu <acm8017@g.rit.edu@cda44fc4-ffb3-9101-b382-7fd7d470b8d5>
2012-11-10 02:45:59 +0000

    Changed arraylist instantiations to implicit and added connect and close statements to post.

---
+++ Test/src/DataLayer/JEncryption.java
@@ -0,0 +1,81 @@
+package DataLayer;
+
+//Taken from http://www.mkyong.com/java/jce-encryption-data-encryption-standard-des-tutorial/
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+ 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+ 
+public class JEncryption
+{    
+        static Cipher desCipher;
+        static SecretKey myDesKey;
+        public JEncryption(){
+            try{   
+                KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+		myDesKey = keygenerator.generateKey();
+ 
+		// Create the cipher 
+		desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+ 
+		// Initialize the cipher for encryption
+		desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+            }catch(  NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e){
+			e.printStackTrace();
+		}
+        }
+        
+        public byte[] encrypt(String text){
+            //sensitive information
+            try{
+                    byte[] byteText = text.getBytes();
+ 
+		    System.out.println("Text [Byte Format] : " + text);
+		    System.out.println("Text : " + new String(text));
+ 
+		    // Encrypt the text
+		    byte[] byteEncrypted = desCipher.doFinal(byteText);
+ 
+		    System.out.println("Text Encryted : " + byteEncrypted);
+                    String textEncrypted = new String(byteEncrypted);
+                    return byteEncrypted;
+		    
+            }catch(IllegalBlockSizeException | BadPaddingException e){
+                return null;
+            }
+                    
+        }
+        
+        public String decrypt(byte[] text){
+            try{
+ 
+		    
+ 
+                    // Initialize the same cipher for decryption
+		    desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
+ 
+		    // Decrypt the text
+		    byte[] textDecrypted = desCipher.doFinal(text);
+ 
+		    System.out.println("Text Decryted : " + new String(textDecrypted));
+                    
+                    return new String(textDecrypted);
+ 
+		}catch(InvalidKeyException | IllegalBlockSizeException | BadPaddingException e){
+                    e.printStackTrace();
+                    return null;
+                }
+        }
+            
+	public static void main(String[] argv) {
+            JEncryption encrypter = new JEncryption();
+            byte[] encryptedText = encrypter.encrypt("Mike is a whore");
+            String decryptedText = encrypter.decrypt(encryptedText);
+	}
+}
